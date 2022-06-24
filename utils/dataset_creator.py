816e130653d170f85c63576ab0d7869d945324b1
tobi1modna <tobia.poppi@virgilio.it>
2022-06-24 10:19:54 +0200

    updated scripts for compatibility with ff++ dataset

--- utils/dataset_creator.py
+++ utils/dataset_creator.py
@@ -7,10 +7,16 @@ import shutil
 def get_args():
     parser = argparse.ArgumentParser("Dataset Creator for DeepFake Detection Task")
     parser.add_argument(
-        "-p",
-        "--path",
+        "-c",
+        "--path-challenge",
         type=str,
-        help="Absolute path the root directory of the single sets.",
+        help="Absolute path the root directory of the single sets (challenge dataset).",
+    )
+    parser.add_argument(
+        "-f",
+        "--path-ff",
+        type=str,
+        help="Absolute path the root directory of the single sets (ff dataset).",
     )
     parser.add_argument(
         "-o",
@@ -38,10 +44,20 @@ def get_args():
 
 def main(opt):
     output_dir = opt.output
-    main_path = Path(opt.path)
+    main_path_challenge = Path(opt.path_challenge)
+    main_path_ff = Path(opt.path_ff)
+
     
-    imgs = sorted(sorted(main_path.rglob('*.png')) + sorted(main_path.rglob('*.jpg')))
+    imgs = sorted(sorted(main_path_challenge.rglob('*.png')) + sorted(main_path_challenge.rglob('*.jpg')) + sorted(main_path_ff.rglob('*.jpg')) + sorted(main_path_ff.rglob('*.png')))
     random.shuffle(imgs)
+    i = 0
+    v = 0
+    for im in imgs:
+        if str(main_path_ff) in str(im):
+            i += 1
+        elif str(main_path_challenge) in str(im):
+            v += 1
+    print(f'{i} immagini ff, {v} immagini challenge')
     
     print('There are ' + str(len(imgs)) + ' images.\n')
     trainval_len = len(imgs) * (100 - int(opt.split_test)) // 100
@@ -82,11 +98,19 @@ def main(opt):
         shutil.copyfile(im, os.path.join(test_dir, str(image_id)+im.suffix))
         label_filename = str(image_id)+'.txt'
         with open(os.path.join(test_dir, label_filename), 'w') as f:
-            f.write(im.parent.stem[0])
-            if im.parent.stem[0] == '0':
-                count_real += 1
-            else:
-                count_fake += 1
+            if str(main_path_challenge) in str(im):        
+                f.write(im.parent.stem[0])
+                if im.parent.stem[0] == '0':
+                    count_real += 1
+                else:
+                    count_fake += 1
+            elif str(main_path_ff) in str(im):
+                if "original" in str(im.stem):
+                    f.write('0')
+                    count_real += 1
+                elif "manipulated" in str(im.stem):
+                    f.write('1')
+                    count_fake += 1
 
         if c%1000 == 0:
             print(str(c)+' elements processed.')
@@ -100,11 +124,19 @@ def main(opt):
         shutil.copyfile(im, os.path.join(train_dir, str(image_id)+im.suffix))
         label_filename = str(image_id)+'.txt'
         with open(os.path.join(train_dir, label_filename), 'w') as f:
-            f.write(im.parent.stem[0])
-            if im.parent.stem[0] == '0':
-                count_real += 1
-            else:
-                count_fake += 1
+            if str(main_path_challenge) in str(im):        
+                f.write(im.parent.stem[0])
+                if im.parent.stem[0] == '0':
+                    count_real += 1
+                else:
+                    count_fake += 1
+            elif str(main_path_ff) in str(im):
+                if "original" in str(im.stem):
+                    f.write('0')
+                    count_real += 1
+                elif "manipulated" in str(im.stem):
+                    f.write('1')
+                    count_fake += 1
         
         if c%1000 == 0:
             print(str(c)+' elements processed.')
@@ -117,12 +149,20 @@ def main(opt):
         shutil.copyfile(im, os.path.join(val_dir, str(image_id)+im.suffix))
         label_filename = str(image_id)+'.txt'
         with open(os.path.join(val_dir, label_filename), 'w') as f:
-            f.write(im.parent.stem[0])
-            if im.parent.stem[0] == '0':
-                count_real += 1
-            else:
-                count_fake += 1
-            
+            if str(main_path_challenge) in str(im):        
+                f.write(im.parent.stem[0])
+                if im.parent.stem[0] == '0':
+                    count_real += 1
+                else:
+                    count_fake += 1
+            elif str(main_path_ff) in str(im):
+                if "original" in str(im.stem):
+                    f.write('0')
+                    count_real += 1
+                elif "manipulated" in str(im.stem):
+                    f.write('1')
+                    count_fake += 1
+
         if c%1000 == 0:
             print(str(c)+' elements processed.')
         image_id += 1
@@ -132,6 +172,102 @@ def main(opt):
 
     print('FINISHED!')
 
+    # elif d_type == 'ff':
+        
+    #     imgs = sorted(sorted(main_path_ff.rglob('*.png')) + sorted(main_path_ff.rglob('*.jpg')))
+    #     random.shuffle(imgs)
+        
+    #     print('There are ' + str(len(imgs)) + ' images.\n')
+    #     trainval_len = len(imgs) * (100 - int(opt.split_test)) // 100
+    #     val_len = trainval_len * (int(opt.split_val)) // 100
+    #     test_len = len(imgs) - trainval_len
+    #     train_len = trainval_len - val_len
+    #     print('Training set: ' + str(train_len) + ' images.')
+    #     print('Validation set: ' + str(val_len) + ' images.')
+    #     print('Test set: ' + str(test_len) + ' images.\n')
+    #     print('STARTING DATASET CREATION...')
+
+    #     if not os.path.exists(output_dir):
+    #         os.makedirs(output_dir)
+
+        
+    #     test_dir = os.path.join(output_dir, 'test')
+    #     train_dir = os.path.join(output_dir, 'train')
+    #     val_dir = os.path.join(output_dir, 'validation')
+
+    #     if not os.path.exists(test_dir):
+    #         os.makedirs(test_dir)    
+        
+    #     if not os.path.exists(train_dir):
+    #         os.makedirs(train_dir)
+
+    #     if not os.path.exists(val_dir):
+    #         os.makedirs(val_dir)
+        
+    #     c = 0
+    #     image_id = 0
+
+    #     count_real = 0
+    #     count_fake = 0
+
+    #     #creation of test_set
+    #     for im in imgs[0:test_len]:
+    #         c += 1
+    #         shutil.copyfile(im, os.path.join(test_dir, str(image_id)+im.suffix))
+    #         label_filename = str(image_id)+'.txt'
+    #         with open(os.path.join(test_dir, label_filename), 'w') as f:
+    #             if "original" in str(im):
+    #                 f.write('0')
+    #                 count_real += 1
+    #             elif "manipulated" in str(im):
+    #                 f.write('1')
+    #                 count_fake += 1
+
+    #         if c%1000 == 0:
+    #             print(str(c)+' elements processed.')
+    #         image_id += 1
+
+
+    #     image_id = 0
+    #     #creation of training_set
+    #     for im in imgs[test_len:test_len+train_len]:
+    #         c += 1
+    #         shutil.copyfile(im, os.path.join(train_dir, str(image_id)+im.suffix))
+    #         label_filename = str(image_id)+'.txt'
+    #         with open(os.path.join(test_dir, label_filename), 'w') as f:
+    #             if "original" in str(im):
+    #                 f.write('0')
+    #                 count_real += 1
+    #             elif "manipulated" in str(im):
+    #                 f.write('1')
+    #                 count_fake += 1
+            
+    #         if c%1000 == 0:
+    #             print(str(c)+' elements processed.')
+    #         image_id += 1
+
+    #     image_id = 0
+    #     #creation of val_set
+    #     for im in imgs[test_len+train_len:test_len+train_len+val_len]:
+    #         c += 1
+    #         shutil.copyfile(im, os.path.join(val_dir, str(image_id)+im.suffix))
+    #         label_filename = str(image_id)+'.txt'
+    #         with open(os.path.join(test_dir, label_filename), 'w') as f:
+    #             if "original" in str(im):
+    #                 f.write('0')
+    #                 count_real += 1
+    #             elif "manipulated" in str(im):
+    #                 f.write('1')
+    #                 count_fake += 1
+                
+    #         if c%1000 == 0:
+    #             print(str(c)+' elements processed.')
+    #         image_id += 1
+        
+    #     print('Real images: ' + str(count_real))
+    #     print('Fake images: ' + str(count_fake))
+
+    #     print('FINISHED!')
 
 
 if __name__ == "__main__":
--- utils/get_frames.py
+++ utils/get_frames.py
@@ -53,8 +53,10 @@ if args.output_directory not in os.listdir('.'):
     os.mkdir('./'+args.output_directory)
 
 num_frames = args.frames
-
+print('Start Processing Manipulated Sequences!\n')
 for dir in os.listdir(f'{input_path}/manipulated_sequences/'):
+    print(f'{dir}\n')
+    count_vid = 0
     for quality in os.listdir(f'{input_path}/manipulated_sequences/{dir}'):
         for video in os.listdir(f'{input_path}/manipulated_sequences/{dir}/{quality}/videos/'):
             last = -1
@@ -65,12 +67,88 @@ for dir in os.listdir(f'{input_path}/manipulated_sequences/'):
             tot_frames = durata * fps
             step = tot_frames / num_frames
             success,image = vidcap.read()
-            count = 0
-            while success:
+            count_frame = 0
+            for i in range(num_frames):
+                vidcap.set(cv2.CAP_PROP_POS_FRAMES, i*step)
                 success,image = vidcap.read()
                 if success:
-                    count += 1
-                if last == -1 or (count-last) >= step:
-                    last = count
-                    cv2.imwrite(f"./{args.output_directory}/{dir}_{quality}_{video[:-4]}__{count}.jpg", image)     # save frame as JPEG file  
+                    count_frame += 1
+                    cv2.imwrite(f"./{args.output_directory}/manipulated_{dir}_{quality}_{video[:-4]}__{count_frame}.jpg", image)     # save frame as JPEG file
+                else:
+                    print('error!')
+
+            count_vid += 1
+            if not count_vid%100:
+                print(f'{count_vid} video processed...')
+print('Finished Original Sequences Processing!')
+        
+###############################################
+###      PROCESSING ORIGINAL SEQUENCES      ###
+###############################################
+
+#original sequences
+print('Start Processing Original Sequences!\n')
+count_vid = 0
+for video in os.listdir(f'{input_path}/original_sequences/c23/videos/'):
+    last = -1
+    path = f'{input_path}/original_sequences/c23/videos/{video}'
+    vidcap = cv2.VideoCapture(path)
+    fps = vidcap.get(cv2.CAP_PROP_FPS)
+    durata = get_length(path)
+    tot_frames = durata * fps
+    step = tot_frames / num_frames
+    success,image = vidcap.read()
+    count_frame = 0
+    
+    for i in range(num_frames):
+        vidcap.set(cv2.CAP_PROP_POS_FRAMES, i*step)
+        success,image = vidcap.read()
+        if success:
+            count_frame += 1
+            cv2.imwrite(f"./{args.output_directory}/original_{quality}_{video[:-4]}__{count_frame}.jpg", image)     # save frame as JPEG file  
+        else:
+            print('error!')
+    
+    count_vid += 1
+    if not count_vid%100:
+        print(f'{count_vid} video processed...')
+print('Finished Original Sequences Processing!')
+
+#deepFakeDetection orignial sequences
+print('Start Processing Original Sequences (DeepFakeDetection)!\n')
+count_vid = 0
+for video in os.listdir(f'{input_path}/original_sequences/actors/c23/videos/'):
+    last = -1
+    path = f'{input_path}/original_sequences/actors/c23/videos/{video}'
+    vidcap = cv2.VideoCapture(path)
+    fps = vidcap.get(cv2.CAP_PROP_FPS)
+    durata = get_length(path)
+    tot_frames = durata * fps
+    step = tot_frames / num_frames
+    success,image = vidcap.read()
+    count_frame = 0
+    
+    for i in range(num_frames):
+        vidcap.set(cv2.CAP_PROP_POS_FRAMES, i*step)
+        success,image = vidcap.read()
+        if success:
+            count_frame += 1
+            cv2.imwrite(f"./{args.output_directory}/original_DFD_{quality}_{video[:-4]}__{count_frame}.jpg", image)     # save frame as JPEG file  
+        else:
+            print('error!')
+    
+    count_vid += 1
+    if not count_vid%100:
+        print(f'{count_vid} video processed...')
+
+print('Finished Original Sequences Processing (DeepFakeDetection)!')
+ 
+            # while success:
+            #     success,image = vidcap.read()
+            #     if success:
+            #         count += 1
+            #     if last == -1 or (count-last) >= step:
+            #         last = count
+            #         cv2.imwrite(f"./{args.output_directory}/original_{quality}_{video[:-4]}__{count}.jpg", image)     # save frame as JPEG file  
             # print(f'frames: {count}, durata: {durata}, fps calcolati: {count/durata}')
+        
