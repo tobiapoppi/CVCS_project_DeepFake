cdf86ed9b4c4a3ae2590e8ceba8639ae5958e5e0
elena <elenagovi0@gmail.com>
2022-06-27 18:30:30 +0200

    corrected dataset_creator

--- xception_detection/test_CNN.py
+++ xception_detection/test_CNN.py
@@ -4,12 +4,15 @@ import torchvision
 from torch.utils.data import DataLoader
 import torch.optim as optim
 from torch.optim import lr_scheduler
+from sklearn.metrics import precision_score, recall_score
 import argparse
 import os
 import cv2
+
 from models import model_selection
 from dataset.transform import xception_default_data_transforms
-from dataset.mydataset import MyDataset	
+from dataset.mydataset import MyDataset
+
 def main():
 	args = parse.parse_args()
 	test_list = args.test_list
@@ -21,6 +24,9 @@ def main():
 	test_dataset_size = len(test_dataset)
 	corrects = 0
 	acc = 0
+	one = 0
+	zero = 0
+	true_zero, false_zero, true_one, false_one = 0, 0, 0, 0
 	#model = torchvision.models.densenet121(num_classes=2)
 	model = model_selection(modelname='xception', num_out_classes=2, dropout=0.5)
 	model.load_state_dict(torch.load(model_path))
@@ -35,9 +41,24 @@ def main():
 			outputs = model(image)
 			_, preds = torch.max(outputs.data, 1)
 			corrects += torch.sum(preds == labels.data).to(torch.float32)
+			true_zero += torch.sum((preds == labels.data)*(preds == 0)).to(torch.float32)
+			false_zero += torch.sum((preds != labels.data)*(preds == 0)).to(torch.float32)
+			true_one += torch.sum((preds == labels.data)*(preds == 1)).to(torch.float32)
+			false_one += torch.sum((preds != labels.data)*(preds == 1)).to(torch.float32)
+			one += torch.sum(preds == 1).to(torch.float32)
+			zero += torch.sum(preds == 0).to(torch.float32)
+			# print('One', one)
+			# print('Zero', zero)
 			print('Iteration Acc {:.4f}'.format(torch.sum(preds == labels.data).to(torch.float32)/batch_size))
 		acc = corrects / test_dataset_size
+		avg_precision = (true_one/(true_one+false_one) + true_zero/(true_zero+false_zero))/2
+		avg_recall = (true_one/(true_one+false_zero) + true_zero/(true_zero+false_one))/2
+
+
 		print('Test Acc: {:.4f}'.format(acc))
+		print('Test Average Precision: {:.4f}'.format(avg_precision))
+		print('Test Average Recall: {:.4f}'.format(avg_recall))
+
 
 
 
