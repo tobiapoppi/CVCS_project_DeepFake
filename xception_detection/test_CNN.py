9f2ad294175c6506cd7068ff970c750e9b856e30
elena <elenagovi0@gmail.com>
2022-06-28 13:46:01 +0200

    corrected dataset_creator

--- xception_detection/test_CNN.py
+++ xception_detection/test_CNN.py
@@ -53,11 +53,19 @@ def main():
 		acc = corrects / test_dataset_size
 		avg_precision = (true_one/(true_one+false_one) + true_zero/(true_zero+false_zero))/2
 		avg_recall = (true_one/(true_one+false_zero) + true_zero/(true_zero+false_one))/2
+		prec0 = true_zero/(true_zero+false_zero)
+		prec1 = true_one/(true_one+false_one)
+		rec0 = true_zero/(true_zero+false_one)
+		rec1 = true_one/(true_one+false_zero)
 
 
 		print('Test Acc: {:.4f}'.format(acc))
-		print('Test Average Precision: {:.4f}'.format(avg_precision))
-		print('Test Average Recall: {:.4f}'.format(avg_recall))
+		# print('Test Average Precision: {:.4f}'.format(avg_precision))
+		# print('Test Average Recall: {:.4f}'.format(avg_recall))
+		print('Precision 0', prec0)
+		print('Precision 1', prec1)
+		print('Recall 0', rec0)
+		print('Recall 1', rec1)
 
 
 
--- xception_detection/train_CNN.py
+++ xception_detection/train_CNN.py
@@ -7,7 +7,8 @@ from torch.optim import lr_scheduler
 import argparse
 import os
 import cv2
-
+from sklearn.utils import class_weight
+import numpy as np
 from models import model_selection
 from mesonet import Meso4, MesoInception4
 from dataset.transform import xception_default_data_transforms
@@ -17,6 +18,16 @@ def main():
 	name = args.name
 	continue_train = args.continue_train
 	train_list = args.train_list
+	zero = 0
+	one = 0
+	print(type(train_list))
+	# for tr in train_list:
+	# 	if tr[-1] == 0:
+	# 		zero = zero+1
+	# 	else:
+	# 		one = one+1
+	# print('class one: ', one)
+	# print('class zero: ', zero)
 	val_list = args.val_list
 	epoches = args.epoches
 	batch_size = args.batch_size
@@ -30,17 +41,30 @@ def main():
 	val_dataset = MyDataset(txt_path=val_list, transform=xception_default_data_transforms['val'])
 	train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True, drop_last=False, num_workers=8)
 	val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=batch_size, shuffle=True, drop_last=False, num_workers=8)
+	zero_counter = 0
+	one_counter = 1
+	labels = []
+	for _, targets in train_loader:
+		for target in targets:
+			zero_counter += int((1-target))
+			one_counter += int((target))
+			labels.append(int(target))
+	print(zero_counter)
+	print(one_counter)
+	classes = np.arange(2)
+	class_weights = class_weight.compute_class_weight(class_weight='balanced', classes=classes, y=np.array(labels))
+	class_weights = torch.tensor(class_weights, dtype=torch.float).cuda()
 	train_dataset_size = len(train_dataset)
 	val_dataset_size = len(val_dataset)
 	model = model_selection(modelname='xception', num_out_classes=2, dropout=0.5)
 	if continue_train:
-		model.load_state_dict(torch.load(model_path, map_location='cpu'))
-	#model = model.cuda()
-	realc, fakec = train_dataset.get_counts()
-	class_weights = [realc, fakec]
-	criterion = nn.CrossEntropyLoss(class_weights)
-	optimizer = optim.Adam(model.parameters(), lr=0.001, betas=(0.9, 0.999), eps=1e-08)
-	scheduler = lr_scheduler.StepLR(optimizer, step_size=5, gamma=0.5)
+		model.load_state_dict(torch.load(model_path))
+	model = model.cuda()
+	#weight = torch.FloatTensor([one_counter/train_dataset_size, 0.5]).cuda()
+	criterion = nn.CrossEntropyLoss()#weight=class_weights) #weight=torch.tensor([1, weight_numb]).cuda())
+	optimizer = optim.SGD(model.parameters(), lr=0.005, momentum=0.9)
+	# optimizer = optim.Adam(model.parameters(), lr=0.001, betas=(0.9, 0.999), eps=1e-08)
+	scheduler = lr_scheduler.StepLR(optimizer, step_size=5, gamma=0.05)
 	model = nn.DataParallel(model)
 	best_model_wts = model.state_dict()
 	best_acc = 0.0
@@ -56,8 +80,8 @@ def main():
 		for (image, labels) in train_loader:
 			iter_loss = 0.0
 			iter_corrects = 0.0
-			#image = image.cuda()
-			#labels = labels.cuda()
+			image = image.cuda()
+			labels = labels.cuda()
 			optimizer.zero_grad()
 			outputs = model(image)
 			_, preds = torch.max(outputs.data, 1)
@@ -69,13 +93,8 @@ def main():
 			iter_corrects = torch.sum(preds == labels.data).to(torch.float32)
 			train_corrects += iter_corrects
 			iteration += 1
-			if not (iteration % 20): #enter only if multiple of 20
-				#the dataset is strongly unbalanced, so we need to check some others metrics like f1-score
-				this_iter_acc = iter_corrects / batch_size
-				this_iter_f1 = f1_loss(outputs, labels)
-				print('iteration {} train loss: {:.4f} Acc: {:.4f} F1-score: {:.4f}'.format(iteration, iter_loss / batch_size, iter_corrects / batch_size, this_iter_f1))
-
-
+			if not (iteration % 40):
+				print('iteration {} train loss: {:.4f} Acc: {:.4f}'.format(iteration, iter_loss / batch_size, iter_corrects / batch_size))
 		epoch_loss = train_loss / train_dataset_size
 		epoch_acc = train_corrects / train_dataset_size
 		print('epoch train loss: {:.4f} Acc: {:.4f}'.format(epoch_loss, epoch_acc))
@@ -104,43 +123,7 @@ def main():
 	torch.save(model.module.state_dict(), os.path.join(output_path, "best.pkl"))
 
 
-def f1_loss(y_true:torch.Tensor, y_pred:torch.Tensor, is_training=False) -> torch.Tensor:
-    '''Calculate F1 score. Can work with gpu tensors
-    
-    The original implmentation is written by Michal Haltuf on Kaggle.
-    
-    Returns
-    -------
-    torch.Tensor
-        `ndim` == 1. 0 <= val <= 1
-    
-    Reference
-    ---------
-    - https://www.kaggle.com/rejpalcz/best-loss-function-for-f1-score-metric
-    - https://scikit-learn.org/stable/modules/generated/sklearn.metrics.f1_score.html#sklearn.metrics.f1_score
-    - https://discuss.pytorch.org/t/calculating-precision-recall-and-f1-score-in-case-of-multi-label-classification/28265/6
-    
-    '''
-    assert y_true.ndim == 1
-    assert y_pred.ndim == 1 or y_pred.ndim == 2
-    
-    if y_pred.ndim == 2:
-        y_pred = y_pred.argmax(dim=1)
-        
-    
-    tp = (y_true * y_pred).sum().to(torch.float32)
-    tn = ((1 - y_true) * (1 - y_pred)).sum().to(torch.float32)
-    fp = ((1 - y_true) * y_pred).sum().to(torch.float32)
-    fn = (y_true * (1 - y_pred)).sum().to(torch.float32)
-    
-    epsilon = 1e-7
-    
-    precision = tp / (tp + fp + epsilon)
-    recall = tp / (tp + fn + epsilon)
-    
-    f1 = 2* (precision*recall) / (precision + recall + epsilon)
-    f1.requires_grad = is_training
-    return f1
+
 
 if __name__ == '__main__':
 	parse = argparse.ArgumentParser(
@@ -149,7 +132,7 @@ if __name__ == '__main__':
 	parse.add_argument('--train_list', '-tl' , type=str, default = './data_list/FaceSwap_c0_train.txt')
 	parse.add_argument('--val_list', '-vl' , type=str, default = './data_list/FaceSwap_c0_val.txt')
 	parse.add_argument('--batch_size', '-bz', type=int, default=64)
-	parse.add_argument('--epoches', '-e', type=int, default='20')
+	parse.add_argument('--epoches', '-e', type=int, default='12')
 	parse.add_argument('--model_name', '-mn', type=str, default='fs_c0_299.pkl')
 	parse.add_argument('--continue_train', type=bool, default=False)
 	parse.add_argument('--model_path', '-mp', type=str, default='./output/df_xception_c0_299/1_df_c0_299.pkl')
