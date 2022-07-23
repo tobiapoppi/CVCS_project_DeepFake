b6a3c8d30a2beaf9d6bc30b7e4090e8c638d1c38
elena <elenagovi0@gmail.com>
2022-07-23 10:40:25 +0200

    corrected dataset_creator

--- xception_detection/xception.py
+++ xception_detection/xception.py
@@ -48,7 +48,7 @@ class SeparableConv2d(nn.Module):
     def __init__(self,in_channels,out_channels,kernel_size=1,stride=1,padding=0,dilation=1,bias=False):
         super(SeparableConv2d,self).__init__()
 
-        self.conv1 = nn.Conv2d(in_channels,in_channels,kernel_size,stride,padding,dilation,groups=int(in_channels/2),bias=bias)
+        self.conv1 = nn.Conv2d(in_channels,in_channels,kernel_size,stride,padding,dilation,groups=in_channels,bias=bias)
         self.pointwise = nn.Conv2d(in_channels,out_channels,1,1,0,1,1,bias=bias)
 
     def forward(self,x):
@@ -61,11 +61,11 @@ class Block(nn.Module):
     def __init__(self,in_filters,out_filters,reps,strides=1,start_with_relu=True,grow_first=True):
         super(Block, self).__init__()
 
-        #if out_filters != in_filters or strides!=1:
-        self.skip = nn.Conv2d(in_filters,out_filters,1,stride=strides, bias=False)
-        self.skipbn = nn.BatchNorm2d(out_filters)
-        #else:
-            #self.skip = nn.Conv2d(in_filters,out_filters,1,stride=strides, bias=False)
+        if out_filters != in_filters or strides!=1:
+            self.skip = nn.Conv2d(in_filters,out_filters,1,stride=strides, bias=False)
+            self.skipbn = nn.BatchNorm2d(out_filters)
+        else:
+            self.skip = nn.Conv2d(in_filters,out_filters,1,stride=strides, bias=False)
 
         self.relu = nn.ReLU(inplace=True)
         rep=[]
